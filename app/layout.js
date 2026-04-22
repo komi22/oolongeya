f320d6f5ab8f0cc31b246a3648a289e9eb2fc34a
komi22 <imvst4@gmail.com>
2026-04-22 23:06:14 +0900

    Replace site font with Pretendard

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,12 @@ import { JetBrains_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
 
 const bodyFont = localFont({
-  src: "./fonts/x12y12pxMaruMinyaHangul.woff2",
+  src: [
+    { path: "./fonts/Pretendard-Regular.woff2", weight: "400", style: "normal" },
+    { path: "./fonts/Pretendard-Medium.woff2", weight: "500", style: "normal" },
+    { path: "./fonts/Pretendard-SemiBold.woff2", weight: "600", style: "normal" },
+    { path: "./fonts/Pretendard-Bold.woff2", weight: "700", style: "normal" },
+  ],
   display: "swap",
   variable: "--font-ui",
 });
